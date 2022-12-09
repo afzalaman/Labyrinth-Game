fa93d8eee2007716b6e9af97ccd002e46ce7a3ad
Ali Afzal <afzal.ali.alld@gmail.com>
2022-12-09 01:58:25 +0100

    Add files via upload

---
+++ LeaderBoard.java
@@ -0,0 +1,49 @@
+package assignment3;
+
+public class LeaderBoard implements Comparable<LeaderBoard>{
+    private final String name;
+    public final int level;
+    public final double time;
+    
+    public LeaderBoard(String name, int level, double time){
+        this.name = name;
+        this.level = level;
+        this.time = time;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + level + " level " + time + " seconds";
+    }
+    
+    /**
+     * Compares two LeaderBoard objects by their level value, then by time value, and finally by name.
+     * @param o second LeaderBoard object
+     * @return a negative value if this is less than the second object, 0 if equal, and positive if more than.
+     */
+    @Override
+    public int compareTo(LeaderBoard o) {
+        int levelDif = -Integer.compare(this.level, o.level);
+        int timeDif = (int) Double.compare(this.time, o.time);
+        int nameDif = this.name.compareTo(o.name);
+        if(levelDif != 0){
+            return levelDif;
+        }
+        else if(timeDif != 0){
+            return timeDif;
+        }
+        else return nameDif;
+    }
+}
